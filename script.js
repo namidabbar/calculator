134fda455f96a59b04348a9735fcb33a3ca0074c
AnamK <119437655+namidabbar@users.noreply.github.com>
2022-12-24 14:55:19 +0000

    converted the functions into arrow functions

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
-function add(a,b){
-    return a+b;
-}
-function subtract(a,b){
-    return a-b;
-}
-function multiply(a,b){
-    return a*b;
-}
-function divide(a,b){
-    return a/b;
-}
+let add=(a,b)=>a+b;
+let subtract=(a,b)=>a-b;
+let multiply=(a,b)=>a*b;
+let divide=(a,b)=>a/b;
+
 
