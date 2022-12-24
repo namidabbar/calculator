f5e77d72985109100b4635d953446be28221b9a4
AnamK <119437655+namidabbar@users.noreply.github.com>
2022-12-24 21:09:02 +0000

    added ids to the button tags & created foreach loop in js to attach onclick eventhandlers

--- script.js
+++ script.js
@@ -3,6 +3,23 @@ let subtract=(a,b)=>a-b;
 let multiply=(a,b)=>a*b;
 let divide=(a,b)=>a/b;
 
-
-
 let operate=(operator,a,b)=>operator(a,b);
+
+const container = document.querySelector('#container');
+const display=container.querySelector('#display')
+const operators=container.querySelector('#operators');
+const numbers=container.querySelector('#numbers');
+
+const buttons =numbers.querySelectorAll('button');
+
+buttons.forEach((button) => {
+
+    // and for each one we add a 'click' listener
+    button.addEventListener('click', displayDigits);
+  });
+let displayarray=new Array();
+function displayDigits(){
+displayarray.push[this.id];
+console.log(displayarray);
+    display.innerHTML = this.id;
+}
